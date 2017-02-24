60464157717491bc966870b1b46c383753e3289c
ZJULothar <zhongyushuo@163.com>
2017-02-24 11:06:20 +0800

    Update hbase_install.sh

--- apps/hadoop/hadoop_test1/scripts/hbase_install.sh
+++ apps/hadoop/hadoop_test1/scripts/hbase_install.sh
@@ -6,23 +6,11 @@
 #  To install HBase
 #
 #####################################################################################
-BUILD_DIR="./"$(tool_get_build_dir $1)
 VERSION="1.3.0"
 INSTALL_DIR="/u01/hbase"
-TARGET_DIR=$(tool_get_first_dirname ${BUILD_DIR})
-SERVER_FILENAME=${BUILD_DIR}/${TARGET_DIR}/hbase-server/target/hbase-server-${VERSION}.jar
+TARGET_DIR=$(tool_get_first_dirname ${INSTALL_DIR})
 
 #######################################################################################
-if [ ! "$(tool_check_exists ${SERVER_FILENAME})"  == 0 ]; then
-      echo "HBase-${VERSION} has not been built successfully"
-      exit -1
-fi
-
-TARGET_DIR=$(tool_get_first_dirname ${INSTALL_DIR})
-if [ "$(tool_check_exists ${INSTALL_DIR}/${TARGET_DIR}/bin/hbase)"  == 0 ]; then
-      echo "HBase-${VERSION} has been installed successfully"
-      exit 0
-fi
 
 ####################################################################################
 # Prepare for install
@@ -32,7 +20,6 @@ $(tool_add_sudo) passwd hbase hbasetest
 $(tool_add_sudo) mkdir -p ${INSTALL_DIR}
 $(tool_add_sudo) chown hbase.$(whoami) ${INSTALL_DIR}
 
-tar -zxvf ${SERVER_FILENAME} -C ${INSTALL_DIR}
 TARGET_DIR=$(tool_get_first_dirname ${INSTALL_DIR})
 source /etc/profile
 echo "Finish install preparation......"
