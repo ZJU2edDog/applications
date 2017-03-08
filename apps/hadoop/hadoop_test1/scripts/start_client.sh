823e3e1979e111b3364e6c0657479e0b4f97ca7f
ZJULothar <zhongyushuo@163.com>
2017-03-08 10:04:28 +0800

    Update start_client.sh

--- apps/hadoop/hadoop_test1/scripts/start_client.sh
+++ apps/hadoop/hadoop_test1/scripts/start_client.sh
@@ -16,12 +16,26 @@ if [ "x${1}" == "xall" ] ; then
 else
     #Update date size scale
     if [ ! -z "${2}" ] ; then
-        sed -i "s/hibench\.scale\.profile.*/hibench\.scale\.profile\ ${2}/g" ${HIBENCH_CMD_DIR}/workloads/${1}/conf/10-${1}-userdefine.conf
-        sed -i "s/hibench\.${1}\.datasize.*//g" ${HIBENCH_CMD_DIR}/workloads/${1}/conf/10-${1}-userdefine.conf
-        sed -i "s/hibench\.workload\.datasize.*//g" ${HIBENCH_CMD_DIR}/workloads/${1}/conf/10-${1}-userdefine.conf
+        sed -i "s/hibench\.scale\.profile.*/hibench\.scale\.profile\ ${2}/g" ${APP_ROOT}/apps/hadoop/hadoop_test1/config/${1}_hibench_hadoop.conf
+        sed -i "s/hibench\.${1}\.datasize.*//g" ${APP_ROOT}/apps/hadoop/hadoop_test1/config/${1}_hibench_hadoop.conf
+        sed -i "s/hibench\.workload\.datasize.*//g" ${APP_ROOT}/apps/hadoop/hadoop_test1/config/${1}_hibench_hadoop.conf
         #echo "hibench.${1}.datasize \${hibench.wordcount.${2}.datasize}" >> ${HIBENCH_CMD_DIR}/workloads/${1}/conf/10-${1}-userdefine.conf
         #echo "hibench.workload.datasize  \${hibench.${1}.datasize}" >> ${HIBENCH_CMD_DIR}/workloads/${1}/conf/10-${1}-userdefine.conf
     fi 
+    #Update Hadoop Config
+${tool_add_sudo} cp ${APP_ROOT}/apps/hadoop/hadoop_test1/config/bayes_hibench_hadoop.conf ${HIBENCH_CMD_DIR}/conf/workloads/bayes/conf/10-baytes-userdefine.conf
+${tool_add_sudo} cp ${APP_ROOT}/apps/hadoop/hadoop_test1/config/join_hibench_hadoop.conf ${HIBENCH_CMD_DIR}/workloads/join/conf/10-join-userdefine.conf
+${tool_add_sudo} cp ${APP_ROOT}/apps/hadoop/hadoop_test1/config/aggregation_hibench_hadoop.conf ${HIBENCH_CMD_DIR}/workloads/aggregation/conf/10-aggregation-userdefine.conf
+${tool_add_sudo} cp ${APP_ROOT}/apps/hadoop/hadoop_test1/config/pagerank_hibench_hadoop.conf ${HIBENCH_CMD_DIR}/workloads/pagerank/conf/10-pagerank-userdefine.conf
+${tool_add_sudo} cp ${APP_ROOT}/apps/hadoop/hadoop_test1/config/sleep_hibench_hadoop.conf ${HIBENCH_CMD_DIR}/workloads/sleep/conf/10-sleep-userdefine.conf
+${tool_add_sudo} cp ${APP_ROOT}/apps/hadoop/hadoop_test1/config/scan_hibench_hadoop.conf ${HIBENCH_CMD_DIR}/workloads/scan/conf/10-scan-userdefine.conf
+${tool_add_sudo} cp ${APP_ROOT}/apps/hadoop/hadoop_test1/config/dfsioe_hibench_hadoop.conf ${HIBENCH_CMD_DIR}/workloads/dfsioe/conf/10-dfsioe-userdefine.conf
+${tool_add_sudo} cp ${APP_ROOT}/apps/hadoop/hadoop_test1/config/wordcount_hibench_hadoop.conf ${HIBENCH_CMD_DIR}/workloads/wordcount/conf/10-wordcount-userdefine.conf
+${tool_add_sudo} cp ${APP_ROOT}/apps/hadoop/hadoop_test1/config/terasort_hibench_hadoop.conf ${HIBENCH_CMD_DIR}/workloads/terasort/conf/10-terasort-userdefine.conf
+${tool_add_sudo} cp ${APP_ROOT}/apps/hadoop/hadoop_test1/config/sort_hibench_hadoop.conf ${HIBENCH_CMD_DIR}/workloads/sort/conf/10-sort-userdefine.conf
+${tool_add_sudo} cp ${APP_ROOT}/apps/hadoop/hadoop_test1/config/kmeans_hibench_hadoop.conf ${HIBENCH_CMD_DIR}/workloads/kmeans/conf/10-kmeans-userdefine.conf
+${tool_add_sudo} cp ${APP_ROOT}/apps/hadoop/hadoop_test1/config/nutchindexing_hibench_hadoop.conf ${HIBENCH_CMD_DIR}/workloads/nutchindexing/conf/10-nuthindexing-userdefine.conf
+ 
  
      case "${1}" in
         "pagerank")
